7fef7d68d0d37083b63451f18f0b96456d10ba25
RisabhKedai <risabh.kedia12@gmail.com>
2023-09-23 21:27:58 +0530

    fixed empty argument related bugs

--- code_command.sh
+++ code_command.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-if [ $((1)) == "" ]; then
+if [ $# == 0 ]; then
 	echo "Enter folder name"
 else 
 	mkdir $1
 	cd $1
-	touch inputf.in
-	touch outputf.in
-	cd "../"
+	touch "inputf.in"
+	touch "outputf.in"
+	cd ..
+	subl -n -a $1
+	subl "$1/inputf.in"
+	subl "$1/outputf.in"
 fi
